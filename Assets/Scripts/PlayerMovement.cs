9bc1a0c311b6b6a3ca5c1516c0d0dab0bc7d7e24
Malcolm Lambrecht <jml1769@rit.edu>
2019-02-20 11:25:37 -0500

    Reorganized the scripts to better split their functions

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    public int leanState; //I used an int here because a bool couldn't store the 3 states and I didnt want to use more than one variable for this
+
+    private void Update () {
+        speed = UpdateSpeed();
+        leanState = UpdateLean(); //we will need a method that takes in lean state to do the visual rotation of the cart
+#if UNITY_EDITOR
+        Debug.Log("Player Speed: " + speed + "\nPlayer Lean: " + leanState);
+#endif
+    }
+
+    //scales the z input from the gamemanager to a reasonable speed
+    private float UpdateSpeed()
+    {
+        //map the value, apparently set up so mapping is uneccessary
+        return GameManager.instance.Zinput;
+    }
+
+        //gets the lean values from game manager, returns -1 for left, 0 for center, and 1 for right
+        private int UpdateLean()
+    {
+        if (GameManager.instance.Xinput <= GameManager.TILT_THRESHHOLD * -0.5)
+        {
+            return -1;
+        } else if (GameManager.instance.Xinput >= GameManager.TILT_THRESHHOLD * 0.5)
+        {
+            return 1;
+        } else
+        {
+            return 0;
+        }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // To manipulate the slider which denotes our speed.
+
+public class UIManager : MonoBehaviour
+{
+
+    public static UIManager instance; // Singleton design pattern
+
+    public Slider speedometer; // the forward back tilting, tracks the z axis
+    public Slider tiltometer; //the left right tilting, tracks the x axis
+    public Text test;
+
+    // Set up the singleton design pattern
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        // Set up the speedometer
+        speedometer.minValue = GameManager.MIN_SPEED;
+        speedometer.maxValue = GameManager.MAX_SPEED;
+
+        //Setup the tiltometer
+        tiltometer.minValue = -GameManager.TILT_THRESHHOLD;
+        tiltometer.maxValue = GameManager.TILT_THRESHHOLD;
+    }
+
+    private void Start()
+    {
+      
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+        test.text = "In editor\nX: " + GameManager.instance.Xinput + "\nY: " + GameManager.instance.Yinput + "\nZ: " + GameManager.instance.Zinput;
+#else //this would be on a phone
+        test.text = "In phone \nX: " + GameManager.instance.Xinput + "\nY: " + GameManager.instance.Yinput + "\nZ: " + GameManager.instance.Zinput;
+        UpdateUI();
+#endif
+    }
+
+    // Update the UI slider.
+    public void UpdateUI()
+    {
+        speedometer.value = GameManager.instance.Zinput;
+        tiltometer.value = GameManager.instance.Xinput;
+    }
+}
